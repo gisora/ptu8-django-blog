d8fd000139047446555d1fdb710ba969d328042b
Giedrius Išora <g.isora@outlook.com>
2023-02-22 13:29:05 +0200

    pridėta paieška, autoriaus pilnas vardas

--- ptu8_blog/blog/admin.py
+++ ptu8_blog/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from . import models
 
@@ -9,7 +10,7 @@ class CommentInline(admin.TabularInline):
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created', 'updated', 'display_comments_count')
+    list_display = ('title', 'display_full_name', 'created', 'updated', 'display_comments_count')
     inlines = (CommentInline, )
     list_filter = ('category', 'created')
 
@@ -17,11 +18,21 @@ class PostAdmin(admin.ModelAdmin):
         ('Heading', {'fields': ('title', ('author', 'status'))}),
         ('Content', {'fields': ('text', 'category')}),
     )    
+    
+    search_fields = ('title', 'author')
 
+    def display_full_name(self, obj):
+        return f"{obj.author.first_name} {obj.author.last_name}"
+    display_full_name.short_description = _('author')
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'post', 'posted')
+    list_display = ('author', 'display_email', 'post', 'posted')
+    list_filter = ('posted',)
+    search_fields = ('author', 'display_email', 'post')
 
+    def display_email(self, obj):
+        return obj.author.email
+    display_email.short_description = _('email')
 
 # Register your models here.
 admin.site.register(models.Category)
