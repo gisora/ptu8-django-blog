510734e8a1238480a687d5036a05e2437ed0a320
Giedrius Išora <g.isora@outlook.com>
2023-03-10 12:47:06 +0200

    db į postgres, pridėtas docker conteinerio įrašas

--- ptu8_blog/ptu8_blog/settings.py
+++ ptu8_blog/ptu8_blog/settings.py
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'ptu8_blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "db",
+        "NAME": "ptu8_blog",
+        "USER": "ptu8_blog",
+        "PASSWORD": "ptu8_blog_nesakysiu",
+        "PORT": 5432,
     }
 }
 
